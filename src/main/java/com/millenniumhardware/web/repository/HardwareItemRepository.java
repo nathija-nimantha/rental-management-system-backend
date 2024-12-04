e90d9643be08ebfb3b78546eed04016929086bd4
nathija-nimantha <nathija.max99@gmail.com>
2024-12-04 11:30:45 +0530

    Updated Repository Classes

--- src/main/java/com/millenniumhardware/web/repository/HardwareItemRepository.java
+++ src/main/java/com/millenniumhardware/web/repository/HardwareItemRepository.java
@@ -2,6 +2,8 @@ package com.millenniumhardware.web.repository;
 
 import com.millenniumhardware.web.entity.HardwareItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HardwareItemRepository extends JpaRepository<HardwareItem, Integer> {
 }
--- src/main/java/com/millenniumhardware/web/repository/RentalRepository.java
+++ src/main/java/com/millenniumhardware/web/repository/RentalRepository.java
@@ -2,6 +2,8 @@ package com.millenniumhardware.web.repository;
 
 import com.millenniumhardware.web.entity.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RentalRepository extends JpaRepository<Rental, Integer> {
 }
